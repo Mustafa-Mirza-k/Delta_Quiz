e0bcfd29f129fbcf60e7cd318a2499c2840ff178
MustafaChamp <80091532+MustafaChamp@users.noreply.github.com>
2021-07-27 01:25:24 +0500

    iOS icons and splash screen added!

--- src/Screens/MainScreen.js
+++ src/Screens/MainScreen.js
@@ -5,17 +5,12 @@ import InternetConnectionAlert from 'react-native-internet-connection-alert';
 import styles from '../stylings';
 const MainScreen = () => {
   return (
-    <InternetConnectionAlert
-      onChange={connectionState => {
-        console.log('Connection State: ', connectionState);
-      }}>
-      <View style={styles.container}>
-        <WebView
-          style={styles.webview}
-          source={{uri: 'https://delta-quiz.herokuapp.com/login'}}
-        />
-      </View>
-    </InternetConnectionAlert>
+    <View style={styles.container}>
+      <WebView
+        style={styles.webview}
+        source={{uri: 'https://delta-quiz.herokuapp.com/login'}}
+      />
+    </View>
   );
 };
 
